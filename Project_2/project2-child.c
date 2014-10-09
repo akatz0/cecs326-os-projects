98e965e13ee27f0a226b28b5839275be2a795532
kristinpeterson <kristinpeterson@me.com>
2014-10-09 07:28:55 +0000

    Adding functional parent, child source

--- Project_2/project2-child.c
+++ Project_2/project2-child.c
@@ -3,28 +3,41 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-// TODO: Add description of project2-child.c main() here
-
+/* 
+* PROGRAM: Project2-child.c
+* 
+* Sleeps for a random time (maximum sleep time being command line argument 1), 
+* then prints a statement based on the child number (argv[4]) which includes
+* the child number, it's PID, and an arithmatic operation based on child number.
+*
+* child number 0: operand 1 (argv[2]) + operand 2 (argv[3]) = sum
+* child number 1: operand 1 (argv[2]) - operand 2 (argv[3]) = difference
+* child number 2: operand 1 (argv[2]) * operand 2 (argv[3]) = product
+* child number 3: operand 1 (argv[2]) / operand 2 (argv[3]) = quotient 
+*		& operand 1 (argv[2]) % operand 2 (argv[3]) = remainder
+*
+*
+*/
 int main(int argc, char *argv[])
 {
 	// Declare variables
 	int child_num, sleep_time, op1, op2;
-	
+
 	// Validate arg amount
 	if(argc != 5) 
 	{
-		printf("\nERROR: Incorrect number of arguments\nUsage: %s [child-num] [sleep-time] [integer-operand-1] [integer-operand-2]\n\n", argv[0]);
+		printf("\nERROR: Incorrect number of arguments\nUsage: %s [sleep-time] [integer-operand-1] [integer-operand-2] [child-num]\n\n", argv[0]);
 		exit(1);		
 	}
 	
 	// Cast args to integer	
-	op1 = atoi(argv[1]);
-	op2 = atoi(argv[2]);
-	child_num = atoi(argv[3]);
-	sleep_time = atoi(argv[4]);
-
-	// Initialize Random Number Generator (using time as seed)
-	// srand((int) time(NULL));
+	sleep_time = atoi(argv[1]);
+	op1 = atoi(argv[2]);
+	op2 = atoi(argv[3]);
+	child_num = atoi(argv[4]);
+	
+	// Initialize Random Number Generator (using pid as seed)
+	srand(getpid());
 	
 	// Randomizing sleep time
 	sleep_time = rand() % sleep_time;
@@ -32,17 +45,25 @@ int main(int argc, char *argv[])
 	// Child sleeps for random sleep_time
 	sleep(sleep_time);
 
-	/* TODO: handle arithmatic operation based on child_num (switch statement) */
+	/* Print out the child number, PID and it's associated arithmatic operation */
 	switch(child_num) {
 		case 0:
-			printf("I am child number %i with PID %ld, the sum is %i", child_num, (long)getpid(), op1 + op2);
+			printf("I am child number %i with PID %ld, the sum is %i\n", child_num, (long)getpid(), op1 + op2);
+			break;
 		case 1:
-			printf("I am child number %i with PID %ld, the difference is %i", child_num, (long)getpid(), op1 - op2);
+			printf("I am child number %i with PID %ld, the difference is %i\n", child_num, (long)getpid(), op1 - op2);
+			break;
 		case 2:
-			printf("I am child number %i with PID %ld, the quotient is %i with a remainder of %i", child_num, (long)getpid(), op1 / op2, op1 % op2);
+			// Handle divide by zero error
+			if(op2 != 0) {
+				printf("I am child number %i with PID %ld, the quotient is %i with a remainder of %i\n", child_num, (long)getpid(), op1 / op2, op1 % op2);
+			} else {
+				printf("ERROR: Cannot divide by 0\n");
+			}
+			break;
 		case 3:
-			printf("I am child number %i with PID %ld, the product is %i", child_num, (long)getpid(), op1 * op2);
-	}
-	
+			printf("I am child number %i with PID %ld, the product is %i\n", child_num, (long)getpid(), op1 * op2);
+			break;
+	}	
 //end main
 }
--- Project_2/project2-parent.c
+++ Project_2/project2-parent.c
@@ -4,54 +4,87 @@
 #include <unistd.h>
 #define MAX_SLEEP 50
 
-// TODO: program description here
-
+/*
+* PROGRAM: project2-parent.c
+*
+* Takes command line arguments for maximum sleep-time T which must be in the range
+* 0 < sleep_time < 50 and two integer operands. The process then prints the two numbers
+* and sleeps for a random time modulo T. Then the process is forked 4 times, if the fork
+* is successfull, the child process makes a call to execlp to execute project2-child.out
+* located in the same directory as project2-parent.out passing a unique child number in 
+* the range [0,3], the operands and T. If the fork or exec calls fail the program 
+* exits with failure.
+*
+*/
 int main(int argc, char *argv[])
 {
 	// Declare variables
-	int sleep_time, op1, op2, child_num;
+	int sleep_time, op1, op2, i, status_ptr;
+        pid_t children[4];
 	
 	// Validate arg amount
 	if(argc != 4) 
 	{
-		printf("\nERROR: Incorrect number of arguments.\nUsage: %s [sleep-time] [integer-operand-1] [integer-operand-2]\n\n", argv[0]);
+		printf("\nERROR: Incorrect number of arguments.\nUsage: %s [sleep-time] [operand-1] [operand-2]\n\n", argv[0]);
 		exit(1);		
 	}
-	
-	// Cast args as integers (will evaluate to zero if no valid conversion can be performed)
-	sleep_time = atoi(argv[1]);
-	op1 = atoi(argv[2]);
-	op2 = atoi(argv[3]);
 
+	/* Error check the command line arguments */
+	/* If they’re all valid initialize */
+	if( (sscanf(argv[1], "%i", &sleep_time) != 1)
+	    || (sscanf(argv[2], "%i", &op1) != 1)
+	    || (sscanf(argv[3], "%i", &op2) != 1)) {
+        	printf("Argument Error: The sleep argument is not an integer\n");
+		printf("Usage: %s [sleep-time] [operand-1] [operand-2]", argv[0]);
+        	exit(1); 
+	}
+	
 	// Validate arg content
 	if(sleep_time < 0 || sleep_time > MAX_SLEEP) 
 	{
 		printf("\nUsage: %s [sleep-time] [integer-operand-1] [integer-operand-2]\n[sleep-time] must be a positve integer less than or equal to %d\n\n", argv[0], MAX_SLEEP);
 		exit(1);		
 	}
-
+	
+	// Cast args as integers
+	sleep_time = atoi(argv[1]);
+	op1 = atoi(argv[2]);
+	op2 = atoi(argv[3]);
+	
+	// Print out the parent info
+	printf("\nI am the parent process, the maximum sleep time is %d and the two numbers are %d and %d\n", sleep_time, op1, op2);
+    
 	// Initialize Random Number Generator (using time as seed)
 	srand((int) time(NULL));
 	
 	// Randomizing sleep time
 	sleep_time = rand() % sleep_time;
-	
-	// Print out the parent info
-	printf("\nI am the parent process, the maximum sleep time is %d and the two numbers are %d and %d\n", MAX_SLEEP, op1, op2);
-    
+
 	// Sleep for randomized sleep_time
 	sleep(sleep_time);
 
-	/* TODO: execute 4 x child processes (project2-child.c) passing child_num, sleep_time, op1 & op2 */
-	for (child_num = 0; child_num < 4; child_num++) {
-		// fork() to create new process and exec to run project2-child.c, passing child_argv as arguments to child process
-		// child_argv[] = {"project2-child", i, sleep_time, op1, op2}
-		if(fork() == 0) {
-			static char *child_argv[] = {"project2-child", op1, op2, child_num, sleep_time, (char *) 0};	
-			printf("Forked child %ld", (long)getpid());
-			execvp("project2-child", child_argv);
+	for (i = 0; i < 4; i++) {
+		// fork() to create child process and have child process run the execlp command
+		// to run project2-child.out, passing argv[1-3] and child_num  as arguments to child process
+		if((children[i] = fork()) == 0) {
+			char child_num[15];
+			snprintf(child_num, sizeof(child_num), "%d", i);
+			printf("Forked child %ld\n", (long)getpid());
+			execlp("./project2-child.out", "project2-child.out", argv[1], argv[2], argv[3], child_num, (char *)NULL);
+			perror("exec failure");
+			exit(1);
+		} else if(children[i] == -1) {
+			perror("\n The fork failed\n");
+			exit(1);
 		}
 	}
+
+	//wait for children to end
+	waitpid(children[0], &status_ptr, 0);
+   	waitpid(children[1], &status_ptr, 0);
+	waitpid(children[2], &status_ptr, 0);
+	waitpid(children[3], &status_ptr, 0);
+	exit(0);
 
 //end main
 }
